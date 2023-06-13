6c735e4fcc9bd21333c6d1da0c479605d17e3135
tangchong123 <2049569030@qq.com>
2023-06-13 22:22:38 +0800

    ..

--- web/js/add_friends.js
+++ web/js/add_friends.js
@@ -1,55 +1,76 @@
 let app = new Vue({ 
     el:"#app",
     data: {
+        controlIndex:null,
         friendsMenu:[
             {
                 "icon":"../imgs/我的.png",
-                "menu":"您的好友"
+                "menu":"您的好友",
+                "id":0
             },
             {
                 "icon":"../imgs/group-add.png",
-                "menu":"添加好友"
+                "menu":"添加好友",
+                "id":1
             },
             {
                 "icon":"../imgs/邮件.png",
-                "menu":"待处理邀请"
+                "menu":"待处理邀请",
+                "id":2
             },
             {
                 "icon":"../imgs/屏蔽.png",
-                "menu":"已屏蔽"
+                "menu":"已屏蔽",
+                "id":3
             },
             {
                 "icon":"../imgs/倒计时.png",
-                "menu":"近期共同游戏玩家"
+                "menu":"近期共同游戏玩家",
+                "id":4
             },
         ],
         focusing: [
             {
                 "icon":"../imgs/我的关注.png",
-                "menu":"关注的玩家"
+                "menu":"关注的玩家",
+                "id":5
             },
             {
                 "icon":"../imgs/通知.png",
-                "menu":"关注的游戏"
+                "menu":"关注的游戏",
+                "id":6
             },
         ],
         myGroups:[
             {
                 "icon":"../imgs/group-add.png",
-                "menu":"您的组"
+                "menu":"您的组",
+                "id":7
             },
             {
                 "icon":"../imgs/邮件.png",
-                "menu":"待处理邀请"
+                "menu":"待处理邀请",
+                "id":8
             },
             {
                 "icon":"../imgs/search.png",
-                "menu":"查找组..."
+                "menu":"查找组...",
+                "id":9
             },
             {
                 "icon":"../imgs/group-add.png",
-                "menu":"创建组..."
+                "menu":"创建组...",
+                "id":10
             },
-         ]
+         ],
+         friendsList:[],
      },
+     created(){
+        this.controlIndex = this.friendsMenu[0].id
+     },
+     methods:{
+        changeIndex(i){
+            this.controlIndex = i 
+        }
+     }
     })
