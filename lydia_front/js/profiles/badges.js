29f645e0c4dc968b4c79b0ed2d6d2dddacf04bda
Lydia <858284357@qq.com>
2023-06-17 10:26:45 +0800

    last

--- lydia_front/js/profiles/badges.js
+++ lydia_front/js/profiles/badges.js
@@ -3,6 +3,10 @@ window.onload = function() {
 
     let avatarCt = document.querySelector(".nav>.avatar>img")
     let nicknameCt = document.querySelector(".nickname")
+    // let nicknameCt1 = document.querySelector(".nicknameCt1")
+    // let nicknameCt2 = document.querySelector(".self_info>.nn")
+    // nicknameCt1.innerHTML = user.nickname
+    // nicknameCt2.innerHTML = user.nickname
     function init() {
         avatarCt.src = user.avatar
         nicknameCt.innerHTML = user.nickname
